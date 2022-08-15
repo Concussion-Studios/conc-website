4f570f2b7281de78324919d5d09631b4c81452ae
ryker <avienandregan@gmail.com>
2022-08-15 14:27:21 -0700

    Updated element slider

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ var imgSlideEl = document.getElementsByClassName("imgSlide");
 var videoEls = document.getElementsByTagName("video");
 var contentFrameEl = document.getElementById("contentFrame");
 
+var imgSlideAllowedToCycle = true;
+
 // Image slider setup
 for (var i = 0; i < imgSlideEl.length; i++) {
     var imgSlideChildren = imgSlideEl[i].children;
@@ -11,8 +13,9 @@ for (var i = 0; i < imgSlideEl.length; i++) {
     /* TODO: Implement dots better. They are currently only indicators of which image it is on. */
 
     createDots(imgSlideChildren, imgSlideEl[0]); // why in the name of god does this for loop return 3 when the imgSlideEl.lenght is 1
-    displayImg(imgSlideChildren, 0); // Display the first image
-    autoCycleImgs(imgSlideChildren, 5000);
+    displayEl(imgSlideChildren, 0); // Display the first image
+
+    setTimeout(()=> { autoCycleImgs(imgSlideChildren, 5000) }, 5000);
 }
 
 function getSiSC(children, opposite = false) { // This function helps us to get only the img elements under .imgSlide in order for us to implement the dots inside of the class itself
@@ -27,7 +30,7 @@ function getSiSC(children, opposite = false) { // This function helps us to get 
     return newC;
 }
 
-function getActiveImg(children) {
+function getActiveEl(children) {
     for (i = 0; i < children.length; i++) {
         if (children[i].style.display == "revert") { // this image is being displayed
             index = i;
@@ -37,7 +40,7 @@ function getActiveImg(children) {
     }
 }
 
-function displayImg(cchildren, index) {
+function displayEl(cchildren, index) {
     children = getSiSC(cchildren);
     for (i = 0; i < children.length; i++) {
         children[i].style.display = "none";
@@ -45,9 +48,25 @@ function displayImg(cchildren, index) {
     children[index].style.display = "revert";
 }
 
-function getDotIndex(parentDiv, dot) {
-    console.log(parentDiv);
-    console.log(dot);
+function onDotClick(event, parentDiv, imgDiv) {
+    var dot = event.target || event.srcElement;
+    var dotIndex = 0;
+    var parentDivChildren = parentDiv.children;
+    var imgDivChildren = imgDiv.children;
+    
+    for (var i = 0; i < parentDivChildren.length; i++) {
+        if (parentDivChildren[i] == dot) {
+            break;
+        } else {
+            dotIndex++;
+        }
+    }
+
+    displayEl(imgDivChildren, dotIndex);
+    setActiveDot(parentDivChildren, dotIndex);
+
+    imgSlideAllowedToCycle = false;
+    setTimeout(()=> { continueCycling(imgDivChildren[getActiveEl(imgDivChildren)], imgDivChildren) }, 30000);
 }
 
 function createDots(children, parentDiv) {
@@ -57,8 +76,14 @@ function createDots(children, parentDiv) {
 
     for (var i = 0; i < children.length; i++) { // Dynamically creates dots based on how many images there are
         window["dot_" + i] = document.createElement("span");
-        window["dot_" + i].classList = "dot";
-        // window["dot_" + i].addEventListener("click", ()=> { displayImg(children, getDotIndex(dotsDiv, this)); });
+
+        if (i == 0) {
+            window["dot_" + i].classList = "dot active";
+        } else {
+            window["dot_" + i].classList = "dot";
+        }
+
+        window["dot_" + i].addEventListener("click", ()=>{ onDotClick(event, dotsDiv, parentDiv); });
 
         dotsDiv.appendChild(window["dot_" + i]);
     }
@@ -79,7 +104,7 @@ function cycleImg(cchildren, amount) {
 
     children = getSiSC(cchildren);
 
-    index = getActiveImg(children);
+    index = getActiveEl(children);
 
     // Clamping
     if (amount < 0) {
@@ -90,14 +115,27 @@ function cycleImg(cchildren, amount) {
 
     setActiveDot(children, index);
 
-    displayImg(children, index);  
+    displayEl(children, index);  
 }
 
 function autoCycleImgs(cchildren, time) {
+    if (!imgSlideAllowedToCycle) {
+        return;
+    }
+
     children = getSiSC(cchildren);
     cycleImg(children, 1);
 
     setTimeout(()=> { autoCycleImgs(children, time) }, time);
+}
+
+function continueCycling(currentElement, children) {
+    if (currentElement.tagName == "VIDEO" && currentElement.paused || currentElement.tagName != "VIDEO") {
+        imgSlideAllowedToCycle = true;
+        autoCycleImgs(children, 5000);
+    }
+
+    setTimeout(()=> { continueCycling(currentElement, children) }, 30000);
 }
 
 function displayFrame(page) {
