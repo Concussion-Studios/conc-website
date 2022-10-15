3f507a71fb978cd5f7906183c55f674a96c18d09
ryker <avienandregan@gmail.com>
2022-10-15 10:20:16 -0700

    QoL; Better resolution support; cleaner code & bug fixes

--- scripts.js
+++ scripts.js
@@ -2,15 +2,25 @@
     Code written by: ryker
 */
 
+var slideshowEls = document.getElementsByClassName("slideshow");
+var titleEl = document.getElementById("title");
+
+/* ---- Initial code ---- */
+
+function init() {
+    titleEl.style.opacity = 1;
+    slideshowThink();
+}
+
 /* ---- Slideshow code ---- */
 
-var slideshowEls = document.body.getElementsByClassName("slideshow");
 if (slideshowEls) {
     var childrenEls = slideshowEls[0].children;
 }
 var slideshowIndex = 0;
 
 function slideshowThink() {
+    childrenEls[0].style.opacity = 1;
     setTimeout( ()=> {slideshowIncr();}, 2500);
 }
 
@@ -33,20 +43,24 @@ function slideshowIncr() {
     slideshowThink();
 }
 
-slideshowThink();
-
 /* ---- Animated icon code ---- */
 
 var iconEl = document.getElementById("icon");
 
-iconEl.addEventListener("mouseover", iconHover);
+if (iconEl) {
+    iconEl.addEventListener("mouseover", iconHover);
+}
 
 function iconHover() {
     iconEl.src = "img/logos/conchighspin.gif";
 }
-
-iconEl.addEventListener("mouseout", iconOut);
+if (iconEl) {
+    iconEl.addEventListener("mouseout", iconOut);
+}
 
 function iconOut() {
     iconEl.src = "img/logos/Logo.png";
 }
+
+
+init();
