baf21d9adf214d38908badfb77c5fdcd2a76da4c
ryker <avienandregan@gmail.com>
2022-10-16 14:36:56 -0700

    Styles; Image compression; Google Analytics

--- scripts.js
+++ scripts.js
@@ -4,12 +4,33 @@
 
 var slideshowEls = document.getElementsByClassName("slideshow");
 var titleEl = document.getElementById("title");
+var headerEls = document.querySelectorAll(".breakHeader");
 
 /* ---- Initial code ---- */
 
 function init() {
     titleEl.style.opacity = 1;
     slideshowThink();
+    observerThink();
+}
+
+/* ---- Observer Transition code ---- */
+
+function observerThink() {
+    setTimeout( ()=> {observerCheck();}, 250);
+}
+
+function observerCheck() {
+    for (var i = 0; i < headerEls.length; i++) {
+        var elStyle = headerEls[i].getBoundingClientRect();
+
+        if (elStyle.top < window.innerHeight && elStyle.bottom >= 0) {
+            // Visible
+            headerEls[i].style.opacity = 1;
+        }
+    }
+
+    observerThink();
 }
 
 /* ---- Slideshow code ---- */
@@ -36,7 +57,7 @@ function slideshowIncr() {
     childrenEls[slideshowIndex].style.opacity = 1;
     slideshowIndex++;
 
-    if (slideshowIndex >= 3) {
+    if (slideshowIndex >= childrenEls.length) {
         slideshowIndex = 0;
     }
 
